cb6bf41fbd19a7c6a80c75b8e6f3e5c79a92f8bc
Manel Ben Salah <manel.bensalah@esprit.tn>
2020-10-30 21:04:20 +0100

    controller departement

---
+++ src/main/java/tn/esprit/spring/controller/RestControllerDepartement.java
@@ -0,0 +1,54 @@
+package tn.esprit.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.services.IContratService;
+import tn.esprit.spring.services.IDepartementService;
+@RestController
+public class RestControllerDepartement {
+	@Autowired
+    IDepartementService idepartmentservice;
+	
+	@PostMapping("/ajoutDepartment")
+	@ResponseBody
+	public int ajouterDepartment(@RequestBody Departement departement) {
+		idepartmentservice.ajouterDepartment(departement);
+		return departement.getId();
+	}
+	
+    @DeleteMapping("/deleteDepartementById/{depId}") 
+	@ResponseBody 
+	public void deleteContratByRef(@PathVariable("depId")int depId)
+	{
+    	idepartmentservice.deleteDepartmentById(depId);
+	}
+    
+    @GetMapping(value = "/getDepartementById/{depId}")
+    @ResponseBody
+	public Departement getDepartementById(@PathVariable("depId") int depId) {
+
+		return idepartmentservice.getDepartmentById(depId);
+	}
+    
+    @GetMapping(value = "/getDepartementById")
+    @ResponseBody
+	public List<Departement> getAllDepartements() {
+
+		return idepartmentservice.getAllDepartements();
+	}
+    
+    
+
+}
